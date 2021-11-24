7ca59731129217310c424d2800e5479c161cbe2e
Tetsuya-Nishikawa <te1823ya@gmail.com>
2021-11-24 15:48:25 +0900

    modify int main()

--- test.c
+++ test.c
@@ -21,10 +21,16 @@ int main(){
     uint8_t* code = mmap(0, CODE_SIZE, PROT_READ | PROT_WRITE | PROT_EXEC, MAP_ANONYMOUS | MAP_PRIVATE, -1, 0);
 
     code[pc++]    = 0x60;       //pushad
+    code[pc++]    = 0x83;            //sub esp, 12
+    code[pc++]    = 0xEC;
+    code[pc++]    = 12;
     code[pc++]    = 0xB8+6;     //mov esi, imm32
     Write(Function, code, &pc); //imm32=Function
     code[pc++]    = 0xFF;       //call rm32
     code[pc++]    = 0xD6;       //rm32=esi
+    code[pc++]    = 0x83;        //add rm32, imm8 (rm32=esp, imm8=12)
+    code[pc++]    = 0xC4;
+    code[pc++]    = 12;
     code[pc++]    = 0x61;       //popad
     code[pc++]    = 0xC3;       //ret
     void (*func)()= (void (*)()) code;
